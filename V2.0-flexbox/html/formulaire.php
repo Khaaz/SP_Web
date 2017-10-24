440d9f0bbce07161265807a1a1315630b4253ab6
Khaazz <louison.bellec@gmail.com>
2017-10-24 19:47:34 +0200

    php update + suppression fichier inutiles

--- V2.0-flexbox/html/formulaire.php
+++ V2.0-flexbox/html/formulaire.php
@@ -4,34 +4,34 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 
 	if(!empty($_POST) ) {
 		$test = 0;
-		$monNom = $_POST["name"];
+		$monNom = $_POST["nom"];
 		echo "Bonjour " . $monNom;
 		echo " ! <br>";
 
-		if(!isset($_POST['name']) || empty($_POST['name']) ) {
+		if(!isset($_POST['nom']) || empty($_POST['nom']) ) {
 			echo "Le champ nom est obligatoire.<br>";
 			$test = 1;
 		}
-			elseif(!filter_var($_POST['name'], FILTER_VALIDATE_REGEXP,array("options"=>array("regexp"=>"/^[a-zA-Z]+[ -]{0,1}[a-zA-Z]+$/")))){
+			elseif(!filter_var($_POST['nom'], FILTER_VALIDATE_REGEXP,array("options"=>array("regexp"=>"/^[a-zA-Z]+[ -]{0,1}[a-zA-Z]+$/")))){
 			echo "Ce n'est pas votre nom !<br>";
 			$test = 1;
 		}
 
-		if(!isset($_POST['mail']) || empty($_POST['mail']) ) {
+		if(!isset($_POST['email']) || empty($_POST['email']) ) {
 			echo "Le champ email est obligatoire.<br>";
 			$test = 1;
 		}
-		elseif(!filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL)){
+		elseif(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
 			echo "Votre adresse email n'est pas valide.<br>";
 			$test = 1;
 		}
 
-		if(!isset($_POST['visit']) || empty($_POST['visit']) ) {
+		if(!isset($_POST['visite']) || empty($_POST['visite']) ) {
 			echo "Le champ visite est obligatoire.<br>";
 			$test = 1;
 		}
 
-		if (!isset($_POST["monmessage"]) || empty($_POST["monmessage"])) {
+		if (!isset($_POST["text"]) || empty($_POST["text"])) {
 		     echo "Le champ message est obligatoire<br>";
 		     $test = 1;
 		}
